ae309420360eb87c920c121d3f7aaaa0424431bd
irfan2202 <irfanapurnomo02@gmail.com>
2025-09-22 11:48:10 +0700

    navbar

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div>
+      <nav className={styles.navbar}>
+        <a href="/" className={styles.brand}>
+          My Porfolio
+        </a>
+        <div className={styles.menu}>
+          <a
+            className={styles.menuButton}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            {menuOpen ? "✖" : "☰"}
+          </a>
+
+          <ul className={`${styles.navItems} ${menuOpen ? styles.show : ""}`}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
